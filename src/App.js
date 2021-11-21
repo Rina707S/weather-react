4b77ac1b376c1ac92c3b28ccbf21cd1a2a01c51d
Rina707S <88797166+Rina707S@users.noreply.github.com>
2021-11-22 01:15:27 +0900

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Weather from "./Weather";
 
@@ -5,9 +7,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
         <Weather />
       </header>
+      <p className="copyright">
+        {" "}
+        <a
+          href="https://github.com/Rina707S/weather-react"
+          alt="github"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open sourced
+        </a>
+        , created by{" "}
+        <a
+          href="https://www.linkedin.com/in/ekaterina-romanenkova-9689b4211/"
+          alt="contactUrl"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Rina R
+        </a>
+      </p>
     </div>
   );
 }
